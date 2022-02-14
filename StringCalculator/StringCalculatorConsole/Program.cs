e17c79c5309bbab58cbb0aa6c753be3fcbb92801
Luca Trevisan <trevisan@euris.it>
2022-02-14 12:47:50 +0100

    Added inversion of control

--- StringCalculator/StringCalculatorConsole/Program.cs
+++ StringCalculator/StringCalculatorConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StringCalculatorConsole.Validators;
 using System;
+using StringCalculatorConsole.IOC;
 
 namespace StringCalculatorConsole
 {
@@ -7,18 +8,18 @@ namespace StringCalculatorConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Inserisci la stringa da calcolare");
-            var read = Console.ReadLine();
-            Console.WriteLine($"Il risultato è => {Calculate(read)}");
+            //Console.WriteLine("Inserisci la stringa da calcolare");
+            //var read = Console.ReadLine();
+            //Console.WriteLine($"Il risultato è => {Calculate(read)}");
             //Console.ReadLine();
         }
-        static int Calculate(string inputString)
-        {
-            var validator = new ValidateNegativeNumbers().SetNext(new ValidateLessThanThousand());
-            var calculator = new Calculator(validator);
+        //static int Calculate(string inputString)
+        //{
+        //    var validator = new ValidateNegativeNumbers().SetNext(new ValidateLessThanThousand());
+        //    var calculator = new Container();
 
-            var result = calculator.Add(inputString);
-            return result;
-        }
+        //    var result = calculator.Add(inputString);
+        //    return result;
+        //}
     }
 }
